e3b19e7a725aa41d695115c55c2b6b06362c4e2c
Bohdan Vezdel <vezdel.studio@gmail.com>
2025-09-14 18:43:46 +0300

    first

---
+++ modal.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openCallbackBtn: document.querySelector("[data-modal-open]"),
+    closeCallbackBtn: document.querySelector("[data-modal-close]"),
+    callbackModal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openCallbackBtn.addEventListener("click", toggleCallbackModal);
+  refs.closeCallbackBtn.addEventListener("click", toggleCallbackModal);
+
+  function toggleCallbackModal() {
+    refs.callbackModal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
